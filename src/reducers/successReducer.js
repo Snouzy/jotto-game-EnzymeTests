9da45115c5997aa0839ae7f93968f9941e1e4d80
Snouzy <mathiasnouzy@gmail.com>
2020-08-31 01:50:41 +0200

    MEP redux

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @Description successReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - action to reduce (qui va l'être)
+ * @returns {boolean} - new success state
+ */
+export default (state, action) => {
+    return null;
+};
