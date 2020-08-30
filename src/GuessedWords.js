c2b83d639b16d655a6c373ac8d3decd81088bd6c
Snouzy <mathiasnouzy@gmail.com>
2020-08-31 00:30:45 +0200

    test words guessed

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -5,6 +5,28 @@ const GuessedWords = (props) => {
     let contents;
     if (props.guessedWords.length === 0) {
         contents = <span data-test="guess-instructions">Try to guess the word</span>;
+    } else {
+        const guessedWordsRows = props.guessedWords.map((word, index) => (
+            <tr data-test="guessed-word" key={index}>
+                <td>{word.guessedWord}</td>
+                <td>{word.letterMatchCount}</td>
+            </tr>
+        ));
+        console.log(guessedWordsRows);
+        contents = (
+            <div data-test="guessed-words">
+                <h3>Guessed Words</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Guess</th>
+                            <th>Matching letters</th>
+                        </tr>
+                    </thead>
+                    <tbody>{guessedWordsRows}</tbody>
+                </table>
+            </div>
+        );
     }
     return <div data-test="component-guessed-word">{contents}</div>;
 };
